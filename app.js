24131f3706863ca7f32e00b1158a8201f0cd7a21
Anna Anderwald <anna.v.anderwald@gmail.com>
2021-06-24 16:54:29 -0400

    removed an extra letter in the app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-c
+
  function plus(number) {                
     return function(plusNumber) {
         return plusNumber += number 
